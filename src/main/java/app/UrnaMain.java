fb5311bb3460471ca25098ba886aebfa49c174a1
kisalto <joshuabinotto@hotmail.com>
2024-09-11 21:31:20 -0300

    first commit

---
+++ src/main/java/app/UrnaMain.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrnaMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrnaMain.class, args);
+	}
+
+}
